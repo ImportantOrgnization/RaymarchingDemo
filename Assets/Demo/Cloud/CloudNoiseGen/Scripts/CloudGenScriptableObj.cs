bdbbd23497457f0c164bf8711f7cdbceb7e5fb66
woo <engineerwoo@outlook.com>
2023-03-26 23:39:37 +0800

    代码创建3d纹理

---
+++ Assets/Demo/Cloud/CloudNoiseGen/Scripts/CloudGenScriptableObj.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "createNoise",menuName = "Create/Gen3DNoiseTexture")]
+public class CloudGenScriptableObj : ScriptableObject
+{
+    public string noiseName = "customNoiseName";
+    public int resolution = 32;
+    public CloudNoiseGen.Mode mode;
+    public CloudNoiseGen.NoiseSettings noiseSettings;
+    
+}
---
+++ Assets/Demo/Cloud/CloudNoiseGen/Scripts/CloudGenScritableObjEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(CloudGenScriptableObj))]
+public class CloudGenScritableObjEditor : Editor
+{
+    private CloudGenScriptableObj obj;
+    private void OnEnable()
+    {
+        obj = target as CloudGenScriptableObj;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Generate Noise Texture"))
+        {
+            var path = AssetDatabase.GetAssetPath(obj);
+            path = path.Replace(obj.name + ".asset", "new3dNoise.asset");
+            CloudNoiseGen.perlin = obj.noiseSettings;
+            CloudNoiseGen.worley = obj.noiseSettings;
+            var t3d = CloudNoiseGen.InitializeNoise( obj.noiseName, obj.resolution, obj.mode);
+            AssetDatabase.CreateAsset(t3d,path);
+            AssetDatabase.Refresh();
+            
+        }
+    }
+}
